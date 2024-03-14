3b34876325e6d802a225b24ef1de598d7c5e59b5
pavan <70502985+Pavanpinisetti14@users.noreply.github.com>
2024-03-14 11:02:58 +0530

    Manual

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+import React, { useState } from "react";
+import ComponentToShow from "./components/ComponentToShow"; // Import the component to be shown
+
+function App() {
+  return (
+    <div>
+      <ComponentToShow />
+    </div>
+  );
+}
+
+export default App;
